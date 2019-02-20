d0e7ce6a1929d5940a50f7cc59424b761fb77804
Kolya Venturi <kolya.venturi@enx3s.com>
2019-02-19 22:36:14 -0700

    Add game / player repositories
    Add global repos

---
+++ server/repositories/index.js
@@ -0,0 +1,7 @@
+// @flow
+
+import GameRepository from './game';
+import PlayerRepository from './player';
+
+export const gameRepository = new GameRepository();
+export const playerRepository = new PlayerRepository();
