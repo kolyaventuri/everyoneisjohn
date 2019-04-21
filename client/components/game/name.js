82610e479ca8ccca6c723c148c5b106a344ccd52
Kolya Venturi <kolya.venturi@enx3s.com>
2019-04-21 13:48:38 -0700

    Style the name

--- client/components/game/name.js
+++ client/components/game/name.js
@@ -2,14 +2,21 @@
 
 import React from 'react';
 
+import styles from './name.scss';
+
 type Props = {|
   value: string
 |};
 
 const Name = ({value}: Props) => (
-  <div>
+  <div className={styles.name}>
     <p>You are</p>
-    <p data-type="name">{value}</p>
+    <p
+      className={styles.text}
+      data-type="name"
+    >
+      {value}
+    </p>
   </div>
 );
 
