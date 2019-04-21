872edbdba6967cc581665475ea8a4f147d000001
Kolya Venturi <kolya.venturi@enx3s.com>
2019-04-21 09:16:31 -0700

    WIP styling

--- client/routes/home.js
+++ client/routes/home.js
@@ -1,15 +1,33 @@
 // @flow
 
 import React from 'react';
+import cx from 'classnames';
 import {Link} from 'react-router-dom';
 
 import JoinInput from '../components/join-input';
 
+import globalStyles from '../sass/global.scss';
+import styles from './home.scss';
+
 const Home = () => (
-  <div>
-    <h1>Everyone is John</h1>
-    <Link to="/game/create">Start a Game</Link>
-    <JoinInput/>
+  <div className={styles.container}>
+    <div className={styles['start-here']}>
+      <h1 className={styles.title}>Everyone is John</h1>
+      <div className={cx(globalStyles.btnGroup, styles.homeGroup)}>
+        <Link
+          to="/game/create"
+          className={globalStyles.btn}
+        >
+          <span>Start a Game</span>
+        </Link>
+        <a
+          href="#"
+          className={globalStyles.btn}
+        >
+          <span>Join a Game</span>
+        </a>
+      </div>
+    </div>
   </div>
 );
 
