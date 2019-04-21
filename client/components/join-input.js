3ad0209013fb69064251098626a2800550dc1b2a
Kolya Venturi <kolya.venturi@enx3s.com>
2019-04-21 10:38:19 -0700

    Home page styling

--- client/components/join-input.js
+++ client/components/join-input.js
@@ -2,8 +2,10 @@
 
 import React from 'react';
 import {push} from 'connected-react-router';
+import {Row, Col} from 'react-flexbox-grid';
 
 import {store} from '../store';
+import styles from './join-input.scss';
 
 type State = {|
   gameId: string
@@ -34,10 +36,25 @@ export default class GameJoinInput extends React.Component<{}, State> {
 
   render() {
     return (
-      <form>
-        <input type="text" onKeyUp={this.handleChange}/>
-        <button type="button" onClick={this.handleJoin}>Join Game</button>
-      </form>
+      <Row>
+        <Col xs={12} md={9}>
+          <input
+            type="text"
+            placeholder="Enter Game Code"
+            className={styles.input}
+            onKeyUp={this.handleChange}
+          />
+        </Col>
+        <Col xs={12} md={3}>
+          <button
+            type="button"
+            className={styles.button}
+            onClick={this.handleJoin}
+          >
+            Join Game
+          </button>
+        </Col>
+      </Row>
     );
   }
 }
--- client/routes/home.js
+++ client/routes/home.js
@@ -1,8 +1,8 @@
 // @flow
 
 import React from 'react';
-import cx from 'classnames';
 import {Link} from 'react-router-dom';
+import {Grid, Row, Col} from 'react-flexbox-grid';
 
 import JoinInput from '../components/join-input';
 
@@ -11,23 +11,41 @@ import styles from './home.scss';
 
 const Home = () => (
   <div className={styles.container}>
-    <div className={styles['start-here']}>
-      <h1 className={styles.title}>Everyone is John</h1>
-      <div className={cx(globalStyles.btnGroup, styles.homeGroup)}>
-        <Link
-          to="/game/create"
-          className={globalStyles.btn}
-        >
-          <span>Start a Game</span>
-        </Link>
-        <a
-          href="#"
-          className={globalStyles.btn}
-        >
-          <span>Join a Game</span>
-        </a>
-      </div>
-    </div>
+    <Grid fluid className={styles['start-here']}>
+      <Row>
+        <Col xs={12}>
+          <h1 className={styles.title}>Everyone is John</h1>
+        </Col>
+      </Row>
+      <Row className={styles.homeGroup}>
+        <Col xs={12} lg={4} lgOffset={4}>
+          <JoinInput/>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} lg={8} lgOffset={2}>
+          <p className={styles.midway}><span>Or</span></p>
+        </Col>
+      </Row>
+      <Row className={styles.start}>
+        <Col xs={12} lg={2} lgOffset={4}>
+          <Link
+            to="/game/create"
+            className={globalStyles.btn}
+          >
+            <span>Start a Game</span>
+          </Link>
+        </Col>
+        <Col xs={12} lg={2}>
+          <Link
+            to="/rules"
+            className={globalStyles.btn}
+          >
+            <span>Read the Rules</span>
+          </Link>
+        </Col>
+      </Row>
+    </Grid>
   </div>
 );
 
