dab669b93e29868557304e01608b559b066db51d
Kolya Venturi <kolya.venturi@enx3s.com>
2019-04-21 19:01:34 -0700

    Style the bidding widget

--- client/components/game/bidding.js
+++ client/components/game/bidding.js
@@ -1,9 +1,12 @@
 // @flow
 
 import React from 'react';
+import cx from 'classnames';
 
 import socket from '../../socket';
 import Ticker from '../gm/ticker';
+import globalStyles from '../../sass/global.scss';
+import styles from './section.scss';
 
 type Props = {|
   max: number
@@ -48,13 +51,15 @@ export default class Bidding extends React.Component<Props, State> {
     return submitted ?
       null :
       (
-        <div>
+        <div className={cx(globalStyles.inline, styles.section)}>
+          <p className={styles.title}>Your Bid:</p>
           <Ticker
             value={value}
             onChange={this.handleChange}
           />
           <button
             type="button"
+            className={cx(globalStyles.btn, styles.button)}
             onClick={this.submitBid}
           >
             Submit Bid
