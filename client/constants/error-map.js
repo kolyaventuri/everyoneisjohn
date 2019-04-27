43a8f31d1417ea50d2aa2be265993343bc58955d
Kolya Venturi <kolya.venturi@enx3s.com>
2019-04-27 15:29:18 -0700

    Add generic error trap

--- client/constants/error-map.js
+++ client/constants/error-map.js
@@ -1,7 +1,10 @@
 // @flow
 
 export default {
+  app: {
+    unhandleException: 'An unknown error occured.'
+  },
   game: {
-    doesntExist: 'The requested game doesn\'t exist'
+    doesntExist: 'The requested game doesn\'t exist.'
   }
 };
