d2077bd0f32ec87854084c18bc34a2fa3439e2c3
Kolya Venturi <kolya.venturi@enx3s.com>
2019-04-27 12:43:05 -0700

    Migrate to browser-env and setup localStorage helper

---
+++ test/client/utils/local-storage.js
@@ -0,0 +1,26 @@
+import test from 'ava';
+import {stub} from 'sinon';
+
+import {get, set} from '../../../client/utils/local-storage';
+
+test('it can set values on localStorage', t => {
+  const value = 'abc';
+  const key = 'def';
+
+  set(key, value);
+
+  const result = window.localStorage.getItem(key);
+
+  t.is(result, value);
+});
+
+test('it can get the values from localStorage', t => {
+  const value = 'abc';
+  const key = 'def';
+
+  window.localStorage.setItem(key, value);
+
+  const result = get(key);
+
+  t.is(result, value);
+});
