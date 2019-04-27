6acd05b067d72a812b5ef0e05b2d2d949184fb82
Kolya Venturi <kolya.venturi@enx3s.com>
2019-04-27 16:22:57 -0700

    Allow ability to change name

--- client/components/game/name.js
+++ client/components/game/name.js
@@ -2,22 +2,40 @@
 
 import React from 'react';
 
+import socket from '../../socket';
+
 import styles from './name.scss';
 
 type Props = {|
   value: string
 |};
 
-const Name = ({value}: Props) => (
-  <div className={styles.name}>
-    <p>You are</p>
-    <p
-      className={styles.text}
-      data-type="name"
-    >
-      {value}
-    </p>
-  </div>
-);
+class Name extends React.Component<Props> {
+  handleChange = (e: SyntheticInputEvent<HTMLInputElement>) => {
+    const {
+      target: {
+        value: name
+      }
+    } = e;
+
+    socket.emit('updatePlayer', {name});
+  }
+
+  render() {
+    const {value} = this.props;
+
+    return (
+      <div className={styles.name}>
+        <p>You are</p>
+        <input
+          className={styles.text}
+          data-type="name"
+          defaultValue={value}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
 
 export default Name;
