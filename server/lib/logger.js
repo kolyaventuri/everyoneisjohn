d100efdc9e9ff8bac655119bd6728222f9365cd5
Kolya Venturi <kolya.venturi@enx3s.com>
2019-04-27 20:13:14 -0700

    Make server loggers noop in test

--- server/lib/logger.js
+++ server/lib/logger.js
@@ -4,4 +4,10 @@ import Logger from 'scriba';
 
 const logger = new Logger({scope: 'SERVER'});
 
+if (process.env.NODE_ENV === 'test') {
+  const noop = () => {};
+  logger.logInfo = noop;
+  logger.logError = noop;
+}
+
 export const {logInfo, logError} = logger;
