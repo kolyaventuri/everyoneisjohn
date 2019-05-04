7a59b9395924c38c2bd13abd81d0f9a7090bee11
Kolya Venturi <kolya.venturi@enx3s.com>
2019-05-03 19:54:04 -0700

    Ensure that destroy event is emitted for players that disconnect while not in a game

--- server/socket/actions/disconnect.js
+++ server/socket/actions/disconnect.js
@@ -1,11 +1,14 @@
 // @flow
 
+import {playerRepository} from '../../repositories';
+
 import type {SocketType} from '..';
 
 const disconnect = (socket: SocketType) => {
-  const {player, game} = socket;
+  const player = playerRepository.find(socket.playerId);
+  const {game} = player;
 
-  if (game || (player && player.game)) {
+  if (game) {
     player.disconnect();
   } else if (player) {
     player.destroy();
