da44dc6a3a793719415dcbf7f4ef4a68982c15c4
Kolya Venturi <kolya.venturi@enx3s.com>
2019-05-16 10:13:35 -0700

    Refactor middlewares to fix issue with redux-logger

--- client/store.js
+++ client/store.js
@@ -2,37 +2,29 @@
 
 import {createStore, applyMiddleware, compose} from 'redux';
 import {routerMiddleware} from 'connected-react-router';
-import thunk from 'redux-thunk';
 import {createLogger} from 'redux-logger';
 import {createBrowserHistory} from 'history';
 import createRootReducer from './reducers';
 
 import type {GameStateType} from './apps/game';
 
-const logger = () => {
-  if (process.env.NODE_ENV === 'production') {
-    return;
-  }
-
-  const reduxLogger = createLogger();
-
-  return reduxLogger;
-};
-
 export const history = createBrowserHistory();
 
 export function configureStore(initialState: GameStateType = {}) {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const middlewares = [
+    routerMiddleware(history)
+  ];
+
+  if (process.env.NODE_ENV !== 'production') {
+    middlewares.push(createLogger());
+  }
 
   return createStore(
     createRootReducer(history),
     initialState,
     composeEnhancers(
-      applyMiddleware(
-        routerMiddleware(history),
-        thunk,
-        logger()
-      )
+      applyMiddleware(...middlewares)
     )
   );
 }
