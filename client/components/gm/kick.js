7121899ae3b1b4925bd00dd85cf4eaad52dbc0ad
Kolya Venturi <kolya.venturi@enx3s.com>
2019-05-19 20:16:05 -0600

    Add GM functionality to kick component

--- client/components/gm/kick.js
+++ client/components/gm/kick.js
@@ -2,12 +2,21 @@
 
 import React from 'react';
 
-type Payload = {|
+import socket from '../../socket';
+
+type Props = {|
   player: string
 |};
 
+const kickPlayer = (player: string) => socket.emit('kickPlayer', {player});
+
 const Kick = ({player}: Props) => (
-  <p data-type="button">X</p>
+  <p
+    data-type="button"
+    onClick={() => kickPlayer(player)}
+  >
+    X
+  </p>
 );
 
 export default Kick;
