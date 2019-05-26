9618bbe30296bd72cd7f398020ff72fae9f19eb4
Kolya Venturi <kolya.venturi@enx3s.com>
2019-05-26 14:29:50 -0700

    Add google analytics tracking

--- client/app.js
+++ client/app.js
@@ -5,6 +5,7 @@ import {render} from 'react-dom';
 import {Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {ConnectedRouter} from 'connected-react-router';
+import ReactGA from 'react-ga';
 
 import {store, history} from './store';
 import App from './components/app';
@@ -15,13 +16,27 @@ import './app.scss';
 import './utils/error-trap';
 /* eslint-enable import/no-unassigned-import */
 
+ReactGA.initialize(process.env.GA_TRACKING);
+
+class Routes extends React.Component {
+  componentDidMount() {
+    ReactGA.pageview(window.location.pathname);
+  }
+
+  render() {
+    return (
+      <ConnectedRouter history={history}>
+        <Router history={history}>
+          <App/>
+        </Router>
+      </ConnectedRouter>
+    );
+  }
+}
+
 render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Router history={history}>
-        <App/>
-      </Router>
-    </ConnectedRouter>
+    <Routes/>
   </Provider>
   // $FlowFixMe
   , document.querySelector('#app'));
--- client/store.js
+++ client/store.js
@@ -4,11 +4,17 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import {routerMiddleware} from 'connected-react-router';
 import {createLogger} from 'redux-logger';
 import {createBrowserHistory} from 'history';
+import ReactGA from 'react-ga';
+
 import createRootReducer from './reducers';
 
 import type {GameStateType} from './apps/game';
 
 export const history = createBrowserHistory();
+history.listen(location => {
+  ReactGA.set({page: location.pathname});
+  ReactGA.pageview(location.pathname);
+});
 
 export function configureStore(initialState: GameStateType = {}) {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
