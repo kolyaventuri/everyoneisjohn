7d7aa36018004f1796e60ba50ce78b81cf1443da
Kolya Venturi <kolya.venturi@enx3s.com>
2019-09-08 13:29:52 -0700

    Add defaults helper to localStorage

--- client/utils/local-storage.js
+++ client/utils/local-storage.js
@@ -5,3 +5,16 @@ const {setItem, getItem} = window.localStorage;
 
 export const set = setItem.bind(localStorage);
 export const get = getItem.bind(localStorage);
+
+export const defaults = (opts: {[string]: any}) => {
+  const keys = Object.keys(opts);
+
+  for (const key of keys) {
+    const data = opts[key];
+    const existing = Boolean(get(key));
+
+    if (!existing) {
+      set(key, data);
+    }
+  }
+};
--- test/client/utils/local-storage.test.js
+++ test/client/utils/local-storage.test.js
@@ -1,6 +1,6 @@
 import test from 'ava';
 
-import {get, set} from '../../../client/utils/local-storage';
+import {get, set, defaults} from '../../../client/utils/local-storage';
 
 test('it can set values on localStorage', t => {
   const value = 'abc';
@@ -22,4 +22,35 @@ test('it can get the values from localStorage', t => {
   const result = get(key);
 
   t.is(result, value);
+});
+
+test('it can set default values', t => {
+  const defaultOpts = {
+    foo: 'bar',
+    bar: 'foo'
+  };
+
+  defaults(defaultOpts);
+
+  const result = get('foo');
+  const result2 = get('bar');
+
+  t.is(result, defaultOpts.foo);
+  t.is(result2, defaultOpts.bar);
+});
+
+test('it does not overwrite with defaults if setting exists', t => {
+  const defaultOpts = {
+    foo: 'bar',
+    bar: 'foo'
+  };
+  const previous = 'abc123';
+  set('foo', previous);
+  defaults(defaultOpts);
+
+  const result = get('foo');
+  const result2 = get('bar');
+
+  t.is(result, previous);
+  t.is(result2, defaultOpts.bar);
 });
