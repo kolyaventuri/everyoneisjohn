afb494f2ce67d0be83acf0b9e143ddbca9627eab
Kolya Venturi <kolya.venturi@enx3s.com>
2019-09-08 12:58:20 -0700

    Play sound when game mode is set to voting

--- client/socket/actions/set-game-mode.js
+++ client/socket/actions/set-game-mode.js
@@ -1,8 +1,21 @@
 // @flow
 
+import playSound from '../../utils/play-sound';
+import {ALERT} from '../../constants/effects';
+import {modes} from '../../constants/game';
 import {store} from '../../store';
 
+const triggers = {
+  [modes.VOTING]: () => playSound(ALERT)
+};
+
 const setGameMode = (mode: string) => {
+  const trigger = triggers[mode];
+
+  if (typeof trigger === 'function') {
+    trigger();
+  }
+
   store.dispatch({
     type: 'SET_GAME_MODE',
     payload: {mode}
