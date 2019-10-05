724ac9104096d425cdb14909bb417398b44d3284
Kolya Venturi <kolya.venturi@enx3s.com>
2019-10-05 14:37:15 -0700

    Add a spinner to the header while retrying connection

---
+++ client/components/spinner.js
@@ -0,0 +1,16 @@
+// @flow
+
+import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faSpinner} from '@fortawesome/pro-regular-svg-icons';
+
+import styles from './spinner.scss';
+
+const Spinner = () => (
+  <FontAwesomeIcon
+    className={styles.spinner}
+    icon={faSpinner}
+  />
+);
+
+export default Spinner;
--- test/client/components/header.test.js
+++ test/client/components/header.test.js
@@ -2,13 +2,17 @@ import test from 'ava';
 import React from 'react';
 import proxyquire from 'proxyquire';
 import {shallow} from 'enzyme';
+import {stub} from 'sinon';
+
+import {CONNECTED, FAILING} from '../../../client/constants/connection-status';
 
 const SoundControl = () => <div/>;
 const Header = proxyquire('../../../client/components/header', {
-  './sound-control': {default: SoundControl}
+  './sound-control': {default: SoundControl},
+  'react-redux': {connect: () => stub().returnsArg(0)}
 }).default;
 
-const render = (props = {}) => shallow(<Header {...props}/>);
+const render = (props = {}) => shallow(<Header connectionStatus={CONNECTED} {...props}/>);
 
 test('it renders a link back to the home page', t => {
   const wrapper = render();
@@ -27,4 +31,18 @@ test('it renders a SoundControl component', t => {
   const control = wrapper.find('SoundControl');
 
   t.is(control.length, 1);
+});
+
+test('it does not render a Spinner by default', t => {
+  const wrapper = render();
+  const spinner = wrapper.find('Spinner');
+
+  t.is(spinner.length, 0);
+});
+
+test('it renders a spinner if the connectionStatus is FAILING', t => {
+  const wrapper = render({connectionStatus: FAILING});
+  const spinner = wrapper.find('Spinner');
+
+  t.is(spinner.length, 1);
 });
